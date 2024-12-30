c6889cbf908c1a2748a6f94ec2187439df333c62
Julio Marodin <julio.marodin@gmail.com>
2024-12-30 19:56:58 -0300

    feat: add model pokemon

---
+++ model/pokemon.py
@@ -0,0 +1,7 @@
+class Pokemon:
+    def __init__(self,nome,foto,tipos,habilidades):
+        self.nome=nome
+        self.foto=foto
+        self.tipos=tipos
+        self.habilidades=habilidades
+       
