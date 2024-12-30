c8a78070d8ad84ef8af06228119d8b50f551c9fc
Julio Marodin <julio.marodin@gmail.com>
2024-12-30 19:58:33 -0300

    feat: add logica da bosca por pokemon

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.globals import request
+import requests
+import json
+from model.pokemon import Pokemon
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html", foto="https://www.formulatv.com/images/series/posters/1100/1106/dest_1.jpg")
+
+@app.route("/buscar", methods=['GET',"POST"])
+def buscar():
+    pokemon = Pokemon(request.form['nome'].lower(),"","","")
+    try:
+        res = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.nome}").text)
+        habilidades = [ability["ability"]["name"].upper() for ability in res["abilities"]]
+        habilidades.sort()
+        pokemon.habilidades=habilidades
+        tipos = [ability["type"]["name"].upper() for ability in res["types"]]
+        pokemon.tipos=tipos
+        result = res['sprites']
+        result = result['front_default']
+        pokemon.foto = res['sprites']['front_default']
+    except:
+        return "Nome incorreto"
+    return render_template("index.html", foto=pokemon.foto, nome=pokemon.nome.upper(), tipos=pokemon.tipos, habilidades=pokemon.habilidades)
 
 if __name__ == "__main__":
     app.run(debug=True)
